41c6eb141b76b08922eb63b1100f3b8dfb8b27cf
Kseniya159 <59954886+Kseniya159@users.noreply.github.com>
2020-01-24 13:41:27 +0300

    Домашнее задание 3
    В третьем задании возникли сложности, так я и не смогла его доделать... 5-ое задание помочь решил помощник, на 6-ое уже просто времени не хватило. Прошу прощения.

---
+++ Задание 3.py
@@ -0,0 +1,22 @@
+# Реализовать функцию my_func(),
+# которая принимает три позиционных аргумента,
+# и возвращает сумму наибольших двух аргументов.
+
+def my_func (var_1,var_2,var_3):
+    result = sum([var_1,var_2,var_3])
+    result_1 = max [var_1,var_2,var_3]
+
+
+
+    return result
+
+result = sum((10,20,30))
+result_1 = max((10,20,30))
+
+
+print(result)
+print(result_1)
+
+
+
+
